48040dd882c4afaa8150373238ce52d786e330f1
IoanaO98 <osan.ioana1998@gmail.com>
2025-05-02 20:30:56 +0300

    Set up inputs for CardComponent to receive external data

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
-
-  constructor() { }
-
+  @Input() title: string = '';
+  @Input() subtitle: string = '';
+  @Input() containerTitle: string = '';
+  @Input() isDisabled: boolean = false;
+  @Input() color: 'blue' | 'green' | 'sunset' | 'red' | 'orange' = 'blue';
+  isActive: boolean = false;
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
-
 }
